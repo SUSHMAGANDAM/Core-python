6a461d846437f4913e7c3100ca715d5c4aa6ff42
SUSHMAGANDAM <99244274+SUSHMAGANDAM@users.noreply.github.com>
2022-03-01 00:12:18 +0530

    Add files via upload

---
+++ core python.py
@@ -0,0 +1,12 @@
+# name="sushma"
+# id="123"
+# salary="1000"
+# print("your name is {} the id is {}  the salary is {}".format(name,id,salary))
+# name=input("enter your name")
+# id=int(input("enter the value"))
+# salary=float(input("enter the value"))
+# print("your name is {} the id is {}  the salary is {}".format(name,id,salary))
+s=1,2,3
+print(sum(s))
+avg=s/3
+print(avg)
