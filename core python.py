65b840227c8c7a8dfa3568c850daa0a7ceb02e16
SUSHMAGANDAM <99244274+SUSHMAGANDAM@users.noreply.github.com>
2022-03-02 00:00:08 +0530

    Add files via upload

--- core python.py
+++ core python.py
@@ -6,7 +6,41 @@
 # id=int(input("enter the value"))
 # salary=float(input("enter the value"))
 # print("your name is {} the id is {}  the salary is {}".format(name,id,salary))
-s=1,2,3
-print(sum(s))
-avg=s/3
-print(avg)
+# for i in range(100,111,2):
+#     print(i)
+# for i in range(100,110):
+#     print(i)
+# m=int(input())
+# n=int(input())
+# for i in range(m,n):
+#      print(i)
+# s=int(input("enter the value:"))
+# for i in range(1,s+1):
+#     print(i)
+# for i in range(1,11):
+#     print(i)
+# s=[5,-2,-2,6,7,8,-3,2,-5]
+# for i in s:
+#     if(i<0):
+#         print(i)
+# s=[5,-2,-2,6,7,8,-3,2,-5]
+# for i in s:
+#     if(i<0):
+#         pass
+#     else:
+#         print(i)
+# a="sushma"
+# b="vaishnavi"
+# print(a+b)
+# print(a,b,sep=" ")
+# s=int(input("enter the number:"))
+# print(s**3)
+# v=int(input("enter the value:"))
+# for i in range(1,v+1):
+#     print(i**3)
+# h=eval(input("enter the value:"))
+# print(h,type(h))
+# print("number=%f"%h)
+h=int(input("enter the value"))
+for k in range(3,h,2):
+    print(k)
